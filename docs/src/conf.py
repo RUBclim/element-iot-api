8fb3fa4b3886aa35f8dcf96387af3e7550d93483
Jonas Kittner <jonas.kittner@rub.de>
2024-08-13 16:49:26 +0200

    add initial version of docs

---
+++ docs/src/conf.py
@@ -0,0 +1,71 @@
+from __future__ import annotations
+
+import os
+import sys
+from datetime import date
+
+# Configuration file for the Sphinx documentation builder.
+#
+# This file only contains a selection of the most common options. For a full
+# list see the documentation:
+# https://www.sphinx-doc.org/en/master/usage/configuration.html
+# -- Path setup --------------------------------------------------------------
+# If extensions (or modules to document with autodoc) are in another directory,
+# add these directories to sys.path here. If the directory is relative to the
+# documentation root, use os.path.abspath to make it absolute, like shown here.
+#
+sys.path.insert(0, os.path.abspath('../..'))
+
+# -- Project information -----------------------------------------------------
+
+project = 'element-api'
+copyright = f'2020 - {date.today().year}, Jonas Kittner & Contributors'
+
+# The full version, including alpha/beta/rc tags
+release = '0.0.0'
+
+
+# -- General configuration ---------------------------------------------------
+
+# Add any Sphinx extension module names here, as strings. They can be
+# extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
+# ones.
+extensions = [
+    'sphinx.ext.autodoc',
+    'sphinx.ext.intersphinx',
+    'sphinx_autodoc_typehints',
+    'myst_parser',
+    'sphinx_copybutton',
+    'sphinx.ext.viewcode',
+    'sphinx_toolbox.decorators',
+]
+autodoc_typehints = 'both'
+typehints_fully_qualified = True
+
+# Add any paths that contain templates here, relative to this directory.
+templates_path = ['_templates']
+
+# List of patterns, relative to source directory, that match files and
+# directories to ignore when looking for source files.
+# This pattern also affects html_static_path and html_extra_path.
+exclude_patterns: list[str] = []
+intersphinx_mapping = {
+    'python': ('https://docs.python.org/3', None),
+    'pandas': ('https://pandas.pydata.org/docs', None),
+}
+
+# -- Options for HTML output -------------------------------------------------
+
+# The theme to use for HTML and HTML Help pages.  See the documentation for
+# a list of builtin themes.
+#
+html_theme = 'furo'
+
+html_title = 'element-api Documentation'
+html_short_title = f'element-api-{release}'
+
+source_suffix = {
+    '.rst': 'restructuredtext',
+    '.txt': 'markdown',
+    '.md': 'markdown',
+}
---
+++ docs/src/index.md
@@ -0,0 +1,29 @@
+# Welcome to element-api's documentation!
+
+## `element`
+
+```{eval-rst}
+.. automodule:: element
+   :members:
+   :undoc-members:
+   :private-members:
+```
+
+## `parsers`
+
+```{eval-rst}
+.. automodule:: element.parsers
+   :members:
+   :undoc-members:
+   :private-members:
+```
+
+
+## `schemas`
+
+```{eval-rst}
+.. automodule:: element.schemas
+   :members:
+   :undoc-members:
+   :private-members:
+```
