4e4d03c3d0756dd055466bad46eee324fda833d8
Jonas Kittner <jonas.kittner@rub.de>
2024-08-15 15:30:47 +0200

    0.1.0

--- docs/src/conf.py
+++ docs/src/conf.py
@@ -19,10 +19,10 @@ sys.path.insert(0, os.path.abspath('../..'))
 # -- Project information -----------------------------------------------------
 
 project = 'element-api'
-copyright = f'2020 - {date.today().year}, Jonas Kittner & Contributors'
+copyright = f'2024 - {date.today().year}, Jonas Kittner & Contributors'
 
 # The full version, including alpha/beta/rc tags
-release = '0.0.0'
+release = '0.1.0'
 
 
 # -- General configuration ---------------------------------------------------
